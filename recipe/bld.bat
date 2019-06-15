c2c57f0eae2899aa280c9b806c09c3642394a9a3
Nathan Wendt <nawendt@ou.edu>
2019-06-14 22:36:48 -0500

    Copy conda libs

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,7 @@
-echo "" > configure.win
-echo "PKG_CPPFLAGS=-I%CONDA_PREFIX%/Library/include" > src/Makevars.win
-echo "PKG_LIBS=-L%CONDA_PREFIX%/Library/lib -lproj" >> src/Makevars.win
+mkdir win32 win64
+copy %CONDA_PREFIX%\Library\lib\proj.a win32
+copy %CONDA_PREFIX%\Library\include\proj_api.h win32
+copy %CONDA_PREFIX%\Library\lib\proj.a win64
+copy %CONDA_PREFIX%\Library\include\proj_api.h win64
 "%R%" CMD INSTALL --build .
 if errorlevel 1 exit 1
