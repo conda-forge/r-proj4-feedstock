2528015d384a9239bdfeb84c6d8f6bf11ec151ab
Nathan Wendt <nawendt@ou.edu>
2019-06-14 23:03:42 -0500

    Try local var again

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,9 @@
-mkdir win32 win64
-copy %CONDA_PREFIX%\Library\lib\proj.a win32
-copy %CONDA_PREFIX%\Library\include\proj_api.h win32
-copy %CONDA_PREFIX%\Library\lib\proj.a win64
-copy %CONDA_PREFIX%\Library\include\proj_api.h win64
+"set LOCAL_SOFT=%CONDA_PREFIX%\Library"
+copy %CONDA_PREFIX%\Library\lib\proj.a .
+REM mkdir win32 win64
+REM copy %CONDA_PREFIX%\Library\lib\proj.a win32
+REM copy %CONDA_PREFIX%\Library\include\proj_api.h win32
+REM copy %CONDA_PREFIX%\Library\lib\proj.a win64
+REM copy %CONDA_PREFIX%\Library\include\proj_api.h win64
 "%R%" CMD INSTALL --build .
 if errorlevel 1 exit 1
