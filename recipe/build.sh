b44532092d6203b111ba5a028dc4274777d42f0d
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-18 16:08:57 -0800

    fix mistake

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,5 +12,6 @@ set -o xtrace -o nounset -o pipefail -o errexit
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
     sed -i "s?\${R_HOME}?${BUILD_PREFIX}?" configure.ac
     autoreconf --force --verbose --install
+fi
 
 $R CMD INSTALL --build . ${R_ARGS:-} || cat config.log
