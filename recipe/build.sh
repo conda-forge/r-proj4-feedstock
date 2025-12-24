a5cdb91ad9baee487abf7e7f17f3ec5b9ce60016
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-24 15:10:26 -0600

    fix cross-compile

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,4 +8,12 @@ export C_INCLUDE_PATH="${PREFIX}/include":${C_INCLUDE_PATH:-}
 export CPLUS_INCLUDE_PATH="${PREFIX}/include":${CPLUS_INCLUDE_PATH:-}
 export DYLD_FALLBACK_LIBRARY_PATH=${PREFIX}/lib
 
-$R CMD INSTALL --build . ${R_ARGS:-} --configure-args="--with-proj-lib=$PREFIX/lib --with-proj-include=$PREFIX/include"
+if [[ ${build_platform} != ${target_platform} ]]; then
+    # Directly supply PKG_CFLAGS and PKG_LIBS when cross-compiling instead of using configure script
+    rm configure
+    sed -e 's?@LIBS@?-lproj?' \
+        -e 's?@CPPFLAGS@?-DUSE_PROJ6_API=1 -D_FORTIFY_SOURCE=2?' \
+        src/Makevars.in > src/Makevars
+fi
+
+$R CMD INSTALL --build . ${R_ARGS:-}
