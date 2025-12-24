4efbdd2495ad59cc518457b1ee6e859e2e785833
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-24 12:02:57 -0600

    allow empty vars

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,10 +2,10 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-export LD_LIBRARY_PATH=$PREFIX/lib/:${LD_LIBRARY_PATH}
-export INCLUDE_PATH="${PREFIX}/include":${INCLUDE_PATH}
-export C_INCLUDE_PATH="${PREFIX}/include":${C_INCLUDE_PATH}
-export CPLUS_INCLUDE_PATH="${PREFIX}/include":${CPLUS_INCLUDE_PATH}
+export LD_LIBRARY_PATH=$PREFIX/lib/:${LD_LIBRARY_PATH:-}
+export INCLUDE_PATH="${PREFIX}/include":${INCLUDE_PATH:-}
+export C_INCLUDE_PATH="${PREFIX}/include":${C_INCLUDE_PATH:-}
+export CPLUS_INCLUDE_PATH="${PREFIX}/include":${CPLUS_INCLUDE_PATH:-}
 export DYLD_FALLBACK_LIBRARY_PATH=${PREFIX}/lib
 
 $R CMD INSTALL --build . ${R_ARGS:-} --configure-args="--with-proj-lib=$PREFIX/lib --with-proj-include=$PREFIX/include"
